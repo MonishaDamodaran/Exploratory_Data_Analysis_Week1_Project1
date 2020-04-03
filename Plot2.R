0bee8a64a3fbc356f837a17d99232d93d5dc56b4
MonishaDamodaran <45434493+MonishaDamodaran@users.noreply.github.com>
2020-04-03 15:08:23 +0530

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,60 @@
+#==============================================================================
+#Load the required packages
+
+library(dplyr)
+library(magrittr)
+library(lubridate)
+library(tidyverse)
+library(reshape)
+
+
+#==============================================================================
+#importing the electricity consumption data
+
+ec = read.table(file.choose(), header = TRUE, sep = ";")
+
+
+#==============================================================================
+#Create a copy of data and check the structure of data
+
+ec_copy = ec
+str(ec_copy)
+
+
+#==============================================================================
+#Extract data dated "1/2/2007" or "2/2/2007"
+
+plot_data = subset(ec_copy, Date == "1/2/2007" | Date == "2/2/2007")
+
+
+#=============================================================================
+#Replace "?" as NA and omit them
+
+plot_data[plot_data == "?"] = NA
+nrow(plot_data)
+plot_data = na.omit(plot_data)
+
+
+#=============================================================================
+#Combine Date and Time column to DateTime of POSIXlt format
+
+
+plot_data$DateTime <- strptime(paste(plot_data$Date, plot_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+plot_data = subset(plot_data, select = -c(Date, Time))
+
+
+
+#============================================================================
+#Convert the Required factor columns to numeric
+
+factor_cols = names(plot_data)[sapply(plot_data, is.factor)]
+plot_data[factor_cols] = as.numeric(as.matrix(plot_data[factor_cols]))
+str(plot_data)
+
+#============================================================================
+#Line chart depicting the pattern of Global_active_power
+
+png("plot2.png", height = 480, width = 480)
+with(plot_data, plot(DateTime, Global_active_power, type = "l"))
+dev.off()
+
---
+++ Plot4.R
@@ -0,0 +1,92 @@
+#==============================================================================
+#Load the required packages
+
+library(dplyr)
+library(magrittr)
+library(lubridate)
+library(tidyverse)
+library(reshape)
+
+
+#==============================================================================
+#importing the electricity consumption data
+
+ec = read.table(file.choose(), header = TRUE, sep = ";")
+
+
+#==============================================================================
+#Create a copy of data and check the structure of data
+
+ec_copy = ec
+str(ec_copy)
+
+
+#==============================================================================
+#Extract data dated "1/2/2007" or "2/2/2007"
+
+plot_data = subset(ec_copy, Date == "1/2/2007" | Date == "2/2/2007")
+
+
+#=============================================================================
+#Replace "?" as NA and omit them
+
+plot_data[plot_data == "?"] = NA
+nrow(plot_data)
+plot_data = na.omit(plot_data)
+
+
+#=============================================================================
+#Combine Date and Time column to DateTime of POSIXlt format
+
+
+plot_data$DateTime <- strptime(paste(plot_data$Date, plot_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+plot_data = subset(plot_data, select = -c(Date, Time))
+
+
+
+#============================================================================
+#Convert the Required factor columns to numeric
+
+factor_cols = names(plot_data)[sapply(plot_data, is.factor)]
+plot_data[factor_cols] = as.numeric(as.matrix(plot_data[factor_cols]))
+str(plot_data)
+
+
+
+
+#=========================================================================
+#Change the data to get plot the sub_metering of numbers 1, 2 and 3 in same
+#plot
+
+submetering = plot_data %>% select(Sub_metering_1:Sub_metering_3, DateTime)
+melt_submetering = melt(submetering, id = "DateTime")
+colnames(melt_submetering)
+
+
+
+#=========================================================================
+#To display all four plots in same window create a space for it
+
+
+png("plot4.png", height = 480, width = 480)
+par(mfrow = c(2, 2), mar = c(5, 4, 4, 2))
+with(plot_data, plot(DateTime, Global_active_power, type = "l",xlab = NA))
+with(plot_data, plot(DateTime, Voltage, type = "l", xlab = "datetime",
+                ylim = c(234, 246)))
+
+with(melt_submetering, plot(DateTime, value, type = "n", xlab = NA,
+                       ylab = "Energy sub metering"))
+with(subset(melt_submetering, variable == "Sub_metering_1"), 
+     lines(DateTime, value))
+with(subset(melt_submetering, variable == "Sub_metering_2"), 
+     lines(DateTime, value, col = "red"))
+with(subset(melt_submetering, variable == "Sub_metering_3"), 
+     lines(DateTime, value, col = "blue"))
+legend("topright", col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_2"),
+       lty = 1, cex = 0.6)
+
+with(plot_data, plot(DateTime, Global_reactive_power, type = "l",
+                xlab = "datetime"))
+                
+dev.off()
